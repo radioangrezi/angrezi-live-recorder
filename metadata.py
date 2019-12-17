fbe14aff7178de868925cf4b885f4f2c3e5b2205
jelko arnds <moin@jelkoarnds.com>
2019-12-17 20:28:22 +0100

    --airtime-config added. cleaned airtime.config file.

--- metadata.py
+++
@@ -1,6 +0,0 @@
-import urllib.request, json 
-from slugify import slugify
-
-with urllib.request.urlopen("http://sourcefabric.airtime.pro/api/live-info") as url:
-    data = json.loads(url.read().decode())
-    print(data['currentShow'][0]['name'])
