3c481a920222990aca005a29a8de5eab30bc92bd
jelko arnds <moin@jelkoarnds.com>
2020-07-21 17:46:16 +0200

    add .environment

---
+++ .environment/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# accessible var DEPLOY_REPO_NAME
+# accessible var DEPLOY_DEST
+# accessible var DEPLOY_FROM
+
+# run as angrezi user
+SERVICE_USER=angrezi
+SERVICE_NAME=angrezi-live-recorder
+
+# remove all files
+sudo rm -rf $DEPLOY_DEST
+sudo mkdir $DEPLOY_DEST
+
+# copy application files
+sudo cp -r $DEPLOY_FROM/. $DEPLOY_DEST
+
+# chown by user
+sudo chown -R $SERVICE_USER:$SERVICE_USER $DEPLOY_DEST
+
+# restart service
+sudo systemctl restart $SERVICE_NAME
+
